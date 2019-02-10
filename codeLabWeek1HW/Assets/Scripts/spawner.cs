318dbde9c7f7d7174933b66906cfa671f9c7523d
jinaroo <sohnjin@gmail.com>
2019-02-10 14:30:41 -0500

    added week 1 hw folder

---
+++ codeLabWeek1HW/Assets/Scripts/spawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnGoodStar", 1, 3); //call spawn after 1 seconds and then every 3 seconds
+		InvokeRepeating("SpawnBadStar", 2, 3);
+	}
+
+	void SpawnGoodStar()
+	{
+		GameObject newGoodStar = Instantiate(Resources.Load<GameObject>("Prefabs/goodStar"));
+		newGoodStar.transform.position = new Vector2(Random.Range(-3,3),Random.Range(-6,6));
+		newGoodStar.transform.Rotate(0, 0, Random.Range(0,360));
+	}
+
+	void SpawnBadStar()
+	{
+		GameObject newBadStar = Instantiate(Resources.Load<GameObject>("Prefabs/badStar"));
+		newBadStar.transform.position = new Vector2(Random.Range(-3,3),Random.Range(-6,6));
+		newBadStar.transform.Rotate(0, 0, Random.Range(0,360));
+	}
+}
